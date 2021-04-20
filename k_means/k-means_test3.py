0558c03e535a01a3b57705ed11dc60ab00c12e57
soonwook34 <soonwook34@gmail.com>
2021-04-20 11:09:57 +0900

    K-means 성능 평가 완료

--- k_means/k-means_test3.py
+++ k_means/k-means_test3.py
@@ -4,10 +4,10 @@ from gensim.models import Word2Vec
 from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
+import warnings
 from sklearn.manifold import TSNE
 import os.path
 import pickle
-import warnings
 
 USE_PREV_MODEL = True
 warnings.filterwarnings("ignore")
@@ -36,7 +36,7 @@ def get_sentence_mean_vector(morphs):
         pass
 
 
-dataset = pd.read_csv("../csv/AM_210329_COVID7.csv", encoding="utf-8")
+dataset = pd.read_csv("../csv/AM_210329_COVID9.csv", encoding="utf-8")
 message_list = dataset["MESSAGE"].values.tolist()
 category_list = dataset["CATEGORY"].values.tolist()
 tokenized_data = []
@@ -67,7 +67,7 @@ dataset["wv"] = dataset["mecab_message"].map(get_sentence_mean_vector)
 print("재난문자 데이터 벡터화 완료")
 # print(dataset.head())
 
-num_clusters = 5
+num_clusters = 3
 word_vectors = dataset.wv.to_list()
 
 kmeans_clustering = KMeans(n_clusters=num_clusters)
@@ -76,7 +76,16 @@ dataset["CLASSIFICATION"] = idx
 
 print(dataset.CATEGORY.value_counts())
 print(dataset.CLASSIFICATION.value_counts())
-print(dataset.head())
+# print(dataset.head())
+# print(len(dataset))
+# print(dataset.loc[dataset["CLASSIFICATION"] == 0][["CATEGORY", "CLASSIFICATION", "MESSAGE"]].head())
+# con_matrix = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+con_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for i in range(0, len(dataset)):
+    con_matrix[dataset["CATEGORY"][i]][dataset["CLASSIFICATION"][i]] += 1
+
+for i in range(0, 3):
+    print(con_matrix[i])
 
 # # 클러스터 시각화
 # X = dataset["wv"].to_list()
@@ -98,3 +107,41 @@ print(dataset.head())
 # tsne_df["user_bio"] = dataset["MESSAGE"].to_list()
 # tsne_df["cluster_no"] = y
 # print(tsne_df.sample(3))
+
+# ALL
+# con_matrix = [[4764, 388, 627, 4168, 53],
+#               [5, 7578, 101, 236, 2],
+#               [1, 117, 2781, 44, 229],
+#               [1, 81, 2221, 97, 57],
+#               [3, 790, 492, 39, 2019]]
+# MAIN
+# con_matrix = [[8356, 774, 870],
+#               [96, 7729, 97],
+#               [70, 1116, 7796]]
+# SUB
+con_matrix = [[2723, 272, 177],
+              [2180, 225, 52],
+              [418, 1037, 1888]]
+accuracy = 0
+for i in range(0, 3):
+    accuracy += con_matrix[i][i]
+accuracy /= len(dataset)
+precision = 0
+recall = 0
+f1_score = 0
+for i in range(0, 3):
+    print(con_matrix[i])
+    TP = con_matrix[i][i]
+    FP = 0
+    FN = 0
+    for j in range(0, 3):
+        if j != i:
+            FP += con_matrix[j][i]
+            FN += con_matrix[i][j]
+    precision += (TP / (TP + FP))
+    recall += (TP / (TP + FN))
+precision /= 3
+recall /= 3
+f1_score = 2 * (precision * recall) / (precision + recall)
+
+print(f"Accuracy : {accuracy:.6f}\nF1-Score : {f1_score:.6f}(Precision : {precision:.6f} Recall : {recall:.6f})")
